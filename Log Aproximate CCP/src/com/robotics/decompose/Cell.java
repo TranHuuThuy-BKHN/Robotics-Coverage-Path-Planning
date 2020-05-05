413b0979eefde5c3bf19b9d972922c7b21a0968d
TranHuuThuy <huuthuy2000@gmail.com>
2020-05-05 23:34:32 +0700

    Modify class Cell and add compute distance from cell to charging station

--- Log Aproximate CCP/src/com/robotics/decompose/Cell.java
+++ Log Aproximate CCP/src/com/robotics/decompose/Cell.java
@@ -1,29 +1,73 @@
 package com.robotics.decompose;
 
+import java.util.HashMap;
+
+class Key {
+    int x;
+    int y;
+
+    public Key(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Key == false) return false;
+        return x == ((Key) obj).x && y == ((Key) obj).y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17; // any prime number
+        result = 31 * result + Integer.valueOf(x).hashCode();
+        result = 31 * result + Integer.valueOf(y).hashCode();
+        return result;
+    }
+}
+
 public class Cell {
+    // map chi số hàng, cột tới cell
+    public static HashMap<Key, Cell> mapCells = new HashMap<>();
 
     /* Tọa đô so với trạm sạc, trạm sạc O(0, 0) */
-    private int x;
-    private int y;
+    int x;
+    int y;
 
     /*true : chướng ngại vật
      false : ô đi được */
     private boolean obtacle;
 
+    private int distance; // khoảng cách từ cell hiện tại tới trạm sạc
 
     public Cell(int x, int y, boolean obtacle) {
         this.x = x;
         this.y = y;
         this.obtacle = obtacle;
+        distance = Integer.MAX_VALUE;
+        mapCells.put(new Key(x, y), this);
     }
 
-    /**
-     * @return contour của ô, nếu là chướng ngại vật trả về -1
-     */
-    public int getContour() {
-        if (obtacle) return -1;
-        else return Math.abs(x) + Math.abs(y);
+    public boolean isObtacle() {
+        return obtacle;
     }
 
+    public int getDistance() {
+        return distance;
+    }
 
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public static void main(String[] args) {
+        Cell c = new Cell(0, 0, false);
+        Cell c2 = new Cell(-1, 0, true);
+        Cell c3 = new Cell(1, 0, false);
+
+        int x = c.x - 1, y = c.y;
+        System.out.println(mapCells.get(new Key(x, y)).isObtacle());
+
+    }
 }
+
--- Log Aproximate CCP/src/com/robotics/decompose/Environment.java
+++ Log Aproximate CCP/src/com/robotics/decompose/Environment.java
@@ -1,6 +1,8 @@
 package com.robotics.decompose;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Environment {
     private ArrayList<Cell> cells;
@@ -9,6 +11,26 @@ public class Environment {
 
     public Environment(ArrayList<Cell> cells) {
         this.cells = cells;
+
+        // tính khoảng các các cell tới trạm sạc, sử dụng BFS
+        Cell s = Cell.mapCells.get(new Key(0, 0)); // trạm sạc
+        s.setDistance(0);
+        LinkedList<Cell> queue = new LinkedList<>();
+        queue.push(s);
+        while (!queue.isEmpty()) {
+            Cell c = queue.poll(); // xóa và trả về phân tử đầu tiên
+            int x = c.x, y = c.y;
+            int xs[] = {x, x, x - 1, x + 1}, ys[] = {y - 1, y + 1, y, y};
+            for (int i = 0; i < xs.length; i++) {
+                Cell nbh = Cell.mapCells.get(new Key(xs[i], ys[i]));
+                if (nbh != null && !nbh.isObtacle() && nbh.getDistance() == Integer.MAX_VALUE) {
+                    nbh.setDistance(c.getDistance() + 1);
+                    queue.push(nbh);
+                }else if(nbh != null && nbh.isObtacle()){
+                    nbh.setDistance(-1);
+                }
+            }
+        }
     }
 
     public Tree getTree() {
@@ -20,5 +42,21 @@ public class Environment {
 
     private Tree convertTree() {
         return null;
+    }
+
+    public static void main(String[] args) {
+        Cell s = new Cell(0, 0, false);
+        Cell c1 = new Cell(0, 1, false);
+
+        Cell c2 = new Cell(1, 0, true);
+
+        Cell c3 = new Cell(1, 1, false);
+        Cell c4 = new Cell(2, 1, false);
+        Cell c5 = new Cell(2, 2, false);
+
+        ArrayList<Cell> cells = new ArrayList<>(Arrays.asList(s, c1, c2, c3, c4, c5));
+        Environment e = new Environment(cells);
+
+        System.out.println(c5.getDistance());
     }
 }
